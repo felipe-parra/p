9fec94d8946e89c454a8e53e3254156a3d2ac9bc
pipepico <fpr29.10@gmail.com>
2018-12-11 01:05:27 -0600

    Add files via upload

--- files_open.py
+++ files_open.py
@@ -4,17 +4,19 @@ import os
 import pandas as pd
 from pandas import ExcelWriter
 
-diccionario_valido = []
+def mes_consulta():
+    mes = input("Ingresa mes a consultar, ej. 2018-01 \t")
+    return mes
 
-def open_file(file_name):
+def open_file(file_name,mes_consulta):
     try:
         doc = minidom.parse(file_name)
     except:
-        print(file_name + "\tArchivo no compatible")
+        #print(file_name + "\tArchivo no compatible")
         return False
 
     doc = minidom.parse(file_name)
-
+    relacionado_cfdi = ''
     datos_cfdi = doc.getElementsByTagName("cfdi:Comprobante")
     for dato in datos_cfdi:
         get_version = dato.getAttribute("Version")
@@ -23,9 +25,24 @@ def open_file(file_name):
             folio_Factura = dato.getAttribute("Folio")
             total_Factura = float(dato.getAttribute("Total"))
             str_total = "{:,.2f}".format(total_Factura)
-
+            if(len(folio_Factura) <= 4):
+                folio_Factura = folio_Factura + "\t"
+            if(len(str_total) < 7):
+                str_total = str_total + "\t"
+            tipo_Factura = dato.getAttribute("TipoDeComprobante")
+            if(tipo_Factura == "I"):
+                tipo_Factura = "INGRESO"
+                relacionado_cfdi = ""
+            elif(tipo_Factura == "E"):
+                tipo_Factura = "EGRESO"
+                datos_relacion = doc.getElementsByTagName("cfdi:CfdiRelacionados")
+                for datoR1 in datos_relacion:
+                    datos_relaciones = doc.getElementsByTagName("cfdi:CfdiRelacionado")
+                    for datoR2 in datos_relaciones:
+                        relacionado_cfdi = datoR2.getAttribute("UUID")
+                        
         else:
-            print(file_name + "\tNo es CFDI Version 3.3")
+            tipo_factura    = ""
             serie_Factura   = ""
             folio_Factura   = ""
             total_Factura   = ""
@@ -33,6 +50,7 @@ def open_file(file_name):
             rfc_emisor      = ""
             uuid_cfdi       = ""
             fecha_timbrado  = ""
+            relacionado_cfdi = ""
             return False
 
     datos_emisor = doc.getElementsByTagName("cfdi:Emisor")
@@ -46,8 +64,6 @@ def open_file(file_name):
     for dato in datos_receptor:
         rfc_receptor    = dato.getAttribute("Rfc")
         nombre_receptor = dato.getAttribute("Nombre")
-        #print(rfc_receptor)
-
 
     datos_Complementos = doc.getElementsByTagName("cfdi:Complemento")
     for dato in datos_Complementos:
@@ -56,11 +72,10 @@ def open_file(file_name):
             uuid_cfdi       = datoTimbrado.getAttribute("UUID")
             fecha_timbrado  = datoTimbrado.getAttribute("FechaTimbrado")
 
-    folio_unido = serie_Factura + folio_Factura
-    escribiendo_en_archivo((rfc_emisor, folio_unido , str_total , fecha_timbrado, uuid_cfdi))
-    #diccionario_valido += [{'RFC':rfc_emisor, 'Folio':serie_Factura + folio_Factura, 'Total': str_total, 'Fecha':fecha_timbrado, 'UUID':uuid_cfdi}]
-    valores_unidos = (rfc_emisor + "\t" + serie_Factura + folio_Factura + "\t" + str_total + "\t" + fecha_timbrado + "\t" + uuid_cfdi)
-    print(valores_unidos)
+    diccionario_valido = {'RFC':rfc_emisor, 'Tipo':tipo_Factura , 'Folio': serie_Factura + folio_Factura, 'Total': str_total, 'Fecha':fecha_timbrado, 'UUID':uuid_cfdi}
+    if(fecha_timbrado[0:7] == mes_consulta):
+        print(rfc_emisor + "\t" + tipo_Factura +"\t" +serie_Factura + folio_Factura + "\t" + str_total + "\t" + fecha_timbrado + "\t" + uuid_cfdi + "\t"+ relacionado_cfdi)
+
 
 
 print("*" * 115)
@@ -68,19 +83,14 @@ def os_operations():
     x = os.getcwdb()
     os.chdir("../")
     print(x)
-def escribiendo_en_archivo(rfc,folio,importe,fecha,uuid):
-    file = open("archivo_factura.txt","w")
-    file.write(rfc,folio,importe,fecha,uuid)
-    file.close()
 
-
-labels = ['RFC','Folio','Importe','Fecha','UUID']
 
 if __name__ == '__main__':
+    valor_mes = mes_consulta()
     contador = 0
     archivos = glob.glob("*.xml")
+    #open_file(archivos[6])
     for i in archivos:
         contador += 1
-        open_file(i)
-
+        open_file(i,valor_mes)
     print("Numero de Archivos consultados:\t", contador)
